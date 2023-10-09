34dd8f6cd752da7a2f6ec410524d83eb1620b3fa
braisd11 <braisdiaz97@gmail.com>
2023-10-09 13:41:04 +0200

    Commit Lunes 9

--- drivers.py
+++ drivers.py
@@ -1,7 +1,8 @@
 import var
 
+
 class Drivers():
-    def validarDNI(self = None):
+    def validarDNI(self=None):
         try:
             dni = var.ui.txtDNI.text()
             dni = dni.upper()
@@ -10,20 +11,19 @@ class Drivers():
             dig_ext = "XYZ"
             reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
             numeros = "1234567890"
-            if len(dni) == 9:           #Comprueba que son 9 caracteres
-                dig_control = dni[8]    #Tomo la letra del DNI
-                dni = dni[:8]           #Tomo los números del DNI
+            if len(dni) == 9:                   # Comprueba que son 9 caracteres
+                dig_control = dni[8]            # Tomo la letra del DNI
+                dni = dni[:8]                   # Tomo los números del DNI
                 if dni[0] in dig_ext:
                     dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
                 if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
                     var.ui.lblValidarDNI.setText('V')
-                    var.ui.lblValidarDNI.setStyleSheet('color:green; background-color:white;')
+                    var.ui.lblValidarDNI.setStyleSheet('color:green;')
                 else:
                     var.ui.lblValidarDNI.setText('X')
-                    var.ui.lblValidarDNI.setStyleSheet('color:red; background-color:white;')
+                    var.ui.lblValidarDNI.setStyleSheet('color:red;')
             else:
                 var.ui.lblValidarDNI.setText('X')
-                var.ui.lblValidarDNI.setStyleSheet('color:red; background-color:white')
+                var.ui.lblValidarDNI.setStyleSheet('color:red;')
         except Exception as error:
             print("error en validar dni ", error)
-
